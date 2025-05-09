36a8515e3d03e2f424cb000c36527a856cba4ae8
DMushka <marjushka@yandex.ru>
2025-05-09 19:51:50 +0300

    hw2

--- lessons/settings.gradle.kts
+++ lessons/settings.gradle.kts
@@ -9,7 +9,13 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "otus-kotlin"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-includeBuild("lessons")
-includeBuild("my-compose-project")
+rootProject.name = "lessons"
+
+include("m1l1-first")
+include("m1l3-func")
+include("m1l4-oop")
+include("m2l1-dsl")
+include("m2l2-coroutines")
+
